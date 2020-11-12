c5f84b43364f9507b35f1382749457bf08800fe7
Aminah Ahmed <aminahahmed@Aminahs-MacBook-Air.local>
2020-11-12 10:20:39 +0000

    question 1

---
+++ practice_python.py
@@ -0,0 +1,8 @@
+# 1. Declare a list with numbers 1 to 5 and add 6 at the end.
+# Create a list making sure to use square brackets and print.
+list=[1,2,3,4,5]
+print(list)
+# Use the append method to add 6 at the end
+list.append(6)
+# Print the new list
+print(list)
